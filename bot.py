572d62832ed0ed9d137c1df374fc583e2f23be9d
Nick Kunets <nik.kunets@gmail.com>
2018-01-28 17:23:53 +0300

    Add logic for repost checks

--- bot.py
+++ bot.py
@@ -26,12 +26,19 @@ def send_image(image_url):
 
 if __name__ == '__main__':
     last_posted_record_ids = dict()
+    posted_reposts = dict()
     for group in config.vk_group_ids:
         last_posted_record_ids[group] = 0
     while True:
         for group in config.vk_group_ids:
             wall_record_data = get_data_from_last_wall_record(group)
             if wall_record_data['id'] > last_posted_record_ids[group]:
+                if 'repost_id' in wall_record_data:
+                    if wall_record_data['repost_id'] in posted_reposts and \
+                            posted_reposts[wall_record_data['repost_id']] == wall_record_data['repost_owner_id']:
+                        continue
+                    else:
+                        posted_reposts[wall_record_data['repost_id']] = wall_record_data['repost_owner_id']
                 send_message(wall_record_data['text'])
                 if 'image' in wall_record_data:
                     send_image(wall_record_data['image'])
--- vk_wall_listener.py
+++ vk_wall_listener.py
@@ -13,13 +13,15 @@ def get_last_wall_record(group_id):
 
 def get_data_from_response(response):
     data_dict = json.loads(response)
-    text = data_dict['response'][1]['text']
-    id = data_dict['response'][1]['id']
+    result = dict()
+    result['text'] = data_dict['response'][1]['text']
+    result['id'] = data_dict['response'][1]['id']
     if 'photo' in data_dict['response'][1]['attachment']:
-        image_url = data_dict['response'][1]['attachment']['photo']['src_xxbig']
-        return {'id': id, 'text': text, 'image': image_url}
-    else:
-        return {'id': id, 'text': text}
+        result['image'] = data_dict['response'][1]['attachment']['photo']['src_big']
+    if 'copy_post_id' in data_dict['response'][1]:
+        result['repost_id'] = data_dict['response'][1]['copy_post_id']
+        result['repost_owner_id'] = data_dict['response'][1]['copy_owner_id']
+    return result
 
 
 def get_data_from_last_wall_record(group_id):
@@ -27,4 +29,4 @@ def get_data_from_last_wall_record(group_id):
 
 
 if __name__ == '__main__':
-    print(get_data_from_response(get_last_wall_record()))
+    print(get_data_from_response(get_last_wall_record(config.vk_group_ids[0])))
