db99ce5f0034b000a9d00c5dc3c3dbf8ea9233d9
Anton Sokolov <anton.sokolov@zoho.eu>
2018-03-04 18:47:56 +0300

    Removed receiveRequest method because it useless All receiveRequest methods usage replaced to sendRequest Added simple VK_Group class with test Some test case renamings

--- tests.py
+++ tests.py
@@ -5,24 +5,15 @@ import requests
 class RequestSender:
     def sendRequestWithParams(self, url, params):
         raise NotImplementedError()
-    def receiveRequestWithParams(self, url, params):
-        raise NotImplementedError()
 
 class FakeRequestSender(RequestSender):
     def sendRequestWithParams(self, url, params):
         self.url = url
         self.params = params
 
-    def receiveRequestWithParams(self, url, params):
-        self.url = url
-        self.params = params
-
 class RealRequestSender(RequestSender):
     def sendRequestWithParams(self, url, params):
         return requests.get(url, params = params)
-
-    def receiveRequestWithParams(self, url, parameters):
-        return requests.get(url, params=parameters).text
 
 class APIConnector:
     def __init__(self, api_url, app_token,
@@ -37,36 +28,67 @@ class VK_APIConnector(APIConnector):
         super().__init__(api_url, app_token, 
                          request_sender)
 
-    def receiveRequest(self, request_name, parameters):
+    def sendRequest(self, request_name, parameters):
         vk_request = self._api_url + request_name
         parameters['access_token'] = self._app_token
 
         return self._request_sender.\
-            receiveRequestWithParams(vk_request, 
+            sendRequestWithParams(vk_request, 
                                      parameters)
 
+class VK_Group():
+    def __init__(self, group_id, vk_connector):
+        self._group_id = group_id
+        self._connector = vk_connector
+        self._request_params = {'owner_id': group_id}
+
+    def getLastWallRecords(self, records_count):
+        requestRecordParams = self._request_params
+        requestRecordParams['count'] = records_count
+
+        return self._connector.\
+                     sendRequest('method/wall.get',
+                                requestRecordParams)
 
 class VK_API_Tester(unittest.TestCase):
-    def test_ReceiveRequest(self):
-        test_sender = FakeRequestSender()
-        test_url = 'https://test.vk.api.com/'
-        test_token = 'test_token'
+    def setUp(self):
+        self.test_sender = FakeRequestSender()
+        self.test_url = 'https://test.vk.api.com/'
+        self.test_token = 'test_token'
+
+        self.test_connector = VK_APIConnector(
+                                    self.test_url,
+                                    self.test_token,
+                                    self.test_sender)
+
+    def test_VKConnectorTokenSet(self):
         test_method = 'method/wall.get'
         test_params = {'a': 1, 'b': 2}
-        test_connector = VK_APIConnector(
-                        test_url,
-                        test_token,
-                        test_sender)
 
-        test_connector.receiveRequest(test_method,
-                                      test_params)
+        self.test_connector.sendRequest(test_method,
+                                           test_params)
 
-        self.assertEqual(test_sender.url,
-                        test_url + test_method)
+        self.assertEqual(self.test_sender.url,
+                         self.test_url + test_method)
 
         self.assertEqual(
-                test_sender.params['access_token'],
-                test_token)
+                self.test_sender.params['access_token'],
+                self.test_token)
+
+    def test_GroupGetWallRecords(self):
+        test_group = VK_Group('test_id', 
+                              self.test_connector)
+
+        test_group.getLastWallRecords(2)
+
+        self.assertEqual(self.test_sender.url,
+                     self.test_url + 'method/wall.get')
+        self.assertEqual(
+                self.test_sender.params['owner_id'],
+                'test_id')
+
+        self.assertEqual(
+                self.test_sender.params['count'], 2)
 
 if __name__ == '__main__':
     unittest.main()
