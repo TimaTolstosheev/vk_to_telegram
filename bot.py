5c94d59df0d865dc8a0fac7a176f821c5d507db3
Timofey Tolstosheev <ttolstoshe001@cee.ema.ad.pwcinternal.com>
2018-04-06 16:49:03 +0300

    trailing whitespace issues fixed

--- bot.py
+++ bot.py
@@ -136,5 +136,5 @@ def bot():
 @app.route('/index')#форма, которая отправляет POST в /bot. Чтобы удобнее тестить
 def index():
     return render_template('index.html')
-    
+
 if __name__== '__main__': app.run(host='0.0.0.0')
